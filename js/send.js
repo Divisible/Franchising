d0c734ed9d37cdd83aaa99ca448df4bdeff9410b
Divisible <poznihirenko.o@gmail.com>
2017-07-18 23:48:05 +0300

    first commit

---
+++ js/send.js
@@ -0,0 +1,8 @@
+//<script>
+//document.getElementById('contact-form').addEventListener('submit', function(evt){
+//      var http = new XMLHttpRequest(), f = this;
+//      evt.preventDefault();
+//      http.open("POST", "/Franchising/send.php", true);
+//      http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+//      http.send("name=" + f.name.value + "&email=" + f.email.value + "&comment=" + f.comment.value);
+//    }, false);
